09bc1ba5767b2e403549fdc745910277d8dac267
Marius Colacioiu <marius.colacioiu@gmail.com>
2013-09-27 15:05:18 +0200

    Done about triangle project part 2.

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError, "Side are not valid." unless valid_triangle?(a, b, c)
+
   if (a == b && a == c)
     return :equilateral
   elsif a == b || a == c || b == c
@@ -21,6 +23,18 @@ def triangle(a, b, c)
   else
     return :scalene
   end
+end
+
+def valid_triangle?(a, b, c)
+  valid = true
+  sides = [a, b, c]
+
+  (0..2).each do |i|
+    valid = false if sides[i] <= 0
+    valid = false if (sides[0] + sides[1] <= sides[2])
+    sides << sides.shift
+  end
+  valid
 end
 
 # Error class used in part 2.  No need to change this code.
